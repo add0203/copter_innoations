fb72175dffe6c29057e1155e47617e133c47f626
addbad618 <ananddhardwivedi05@gmail.com>
2024-10-30 12:57:43 +0530

    update files

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,26 +1,10 @@
 "use client";
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import { ReactNode, useState } from "react";
 
-// export default function Provider({ children }: { children: ReactNode }) {
-//   const [queryClient] = useState(() => new QueryClient());
-
-//   return (
-//     <QueryClientProvider client={queryClient}>
-//       <ReactQueryDevtools initialIsOpen={false} />
-//       {children}
-//     </QueryClientProvider>
-//   );
-// }
-// Provider.tsx
-import { Provider } from 'react-redux';
-import { store } from '../redux/store'; // Import the Redux store
+import { Provider } from "react-redux";
+import { store } from "../redux/store"; // Import the Redux store
 
 export default function AppProvider({ children }: { children: ReactNode }) {
-  return (
-    <Provider store={store}>
-      {children}
-    </Provider>
-  );
+  return <Provider store={store}>{children}</Provider>;
 }
